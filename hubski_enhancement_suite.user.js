20620ffe44492616b29c316cbdc7bf4d86b70827
Josh Parnham <joshparnham@gmail.com>
2014-01-18 17:02:00 +1100

    Bump version number of user script to 0.3

--- hubski_enhancement_suite.user.js
+++ hubski_enhancement_suite.user.js
@@ -7,7 +7,7 @@
 // @author        joshparnham
 // @include       http://hubski.com/*
 // @include       https://hubski.com/*
-// @version       0.2.2
+// @version       0.3
 // ==/UserScript==
 
 /*    
